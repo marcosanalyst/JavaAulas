eff381045f163c9388c87a2f421e4afd73251562
Marcos Silva <marcos.silva@yaman.com.br>
2025-04-09 10:31:50 -0300

    Equals

---
+++ conceitos_exercicios_java/src/classe/Equals.java
@@ -0,0 +1,29 @@
+package classe;
+
+import java.util.Date;
+
+public class Equals {
+	
+	public static void main(String[] args) {
+		
+		Usuario u1 = new Usuario();
+		u1.nome = "Pedro Silva";
+		u1.email = "pedro.silva@ex.com.br";
+		
+		Usuario u2 = new Usuario();
+		u2.nome = "Pedro Silva";
+		u2.email = "pedro.silva@ex.com.br";
+		
+		System.out.println(u1 == u2); // falso, comparação de endereço de memória
+		System.out.println(u1.equals(u2)); // falso, equals fará a mesma coisa que ==
+		System.out.println(u2.equals(u1)); // falso
+		System.out.println(u2.equals(u2)); // verdadeiro
+		System.out.println(u1.equals(u1)); // verdadeiro
+		
+		// resultados acima, antes da implementação do equals
+		
+	//	System.out.println(u2.equals(new Date())); // false
+		
+	}
+
+}
