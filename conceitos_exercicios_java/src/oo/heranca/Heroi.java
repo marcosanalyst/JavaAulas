3376afbd3dfdfd91db03ead5b6e462af4bc1b1de
Marcos Silva <marcos.silva@yaman.com.br>
2025-04-11 15:09:51 -0300

    Herança: sobrescrevendo um método

--- conceitos_exercicios_java/src/oo/heranca/Heroi.java
+++ conceitos_exercicios_java/src/oo/heranca/Heroi.java
@@ -1,5 +1,17 @@
 package oo.heranca;
 // Heroi vai receber código para reuso a partir do mecânismo de Herança
 public class Heroi extends Jogador{
+	
+	// sobrescrevendo o comportamento
+	
+	boolean atacar(Jogador oponente) {
+		boolean ataque1 = super.atacar(oponente);
+		boolean ataque2 = super.atacar(oponente);
+		boolean ataque3 = super.atacar(oponente);
+	
+		return ataque1 || ataque2 || ataque3;
+		}
+		
+	}
 
-}
+
