147738b1b5ae32dccbf9db560c41d79d492e0a96
Marcos Silva <marcos.silva@yaman.com.br>
2025-04-14 17:45:56 -0300

    Polimosfismo estático! Sobrecarga de métodos!

---
+++ conceitos_exercicios_java/src/oo/polimosfismo/Feijao.java
@@ -0,0 +1,23 @@
+package oo.polimosfismo;
+
+public class Feijao {
+	
+	private double peso;
+	
+	public Feijao(double peso) {
+		setPeso(peso);
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		if(peso >= 0)
+		this.peso = peso;
+	}
+	
+}
+	
+
+
